e0d7e22f1185407e0a199957ffce52fe2f27c489
Deanna Earley <dee@earlsoft.co.uk>
2020-07-03 00:37:50 +0100

    Allow resetting the camera to the player position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,26 +15,38 @@ public class PlayerController : MonoBehaviour {
 
     private float fallSpeed = 0;
 
+    private Transform mainObject = null;
+    private Transform offsetObject = null;
+    private Transform cameraObject = null;
+
     void Start () {
         // Get the character controller
         this.characterController = this.GetComponent<CharacterController> ();
+
+        // Get the object that defines our forward direction
+        this.mainObject = this.transform;
+        this.offsetObject = mainObject.Find ("Camera offset");
+        this.cameraObject = this.offsetObject.Find ("Main Camera");
+
+        if (this.cameraForward) { this.mainObject = this.cameraObject; }
     }
 
     void Update () {
+        // Reset camera position to match the player object if the Reset input is pressed
+        if (Input.GetButtonDown ("Reset")) {
+            this.ResetOffset ();
+        }
+
         // Get input data from keyboard or controller
         float forwardSpeed = Input.GetAxis ("Vertical");
         float sidewaysSpeed = Input.GetAxis ("Horizontal");
         float rotationalSpeed = Input.GetAxis ("Horizontal2");
 
-        // Get the object that defines our forward direction
-        Transform mainObject = this.transform;
-        if (this.cameraForward) { mainObject = mainObject.Find ("Main Camera"); }
-
         // Move forward and sideways, making sure we stay on the ground
         Vector3 moveDirection = Vector3.zero;
         if (this.characterController.isGrounded) {
-            moveDirection += mainObject.forward * forwardSpeed;
-            moveDirection += mainObject.right * sidewaysSpeed;
+            moveDirection += this.mainObject.forward * forwardSpeed;
+            moveDirection += this.mainObject.right * sidewaysSpeed;
             moveDirection *= this.moveSpeed * Time.deltaTime;
             // No flying!
             this.fallSpeed = 0;
@@ -50,5 +62,23 @@ public class PlayerController : MonoBehaviour {
         Vector3 rotateDirection = new Vector3 (0, rotationalSpeed, 0);
         rotateDirection *= this.rotateSpeed * Time.deltaTime;
         this.transform.Rotate (rotateDirection, Space.Self);
+    }
+
+    /// <summary>
+    /// Resets the camera position to the center of the player object  
+    /// </summary>
+    void ResetOffset () {
+        // Get the camera's current position
+        Vector3 offsetPosition = this.cameraObject.localPosition;
+
+        // Create the required offset values
+        offsetPosition = new Vector3 (
+            -offsetPosition.x,
+            0, // Camera should stay at head height
+            -offsetPosition.z
+        );
+
+        // Change the offset
+        this.offsetObject.localPosition = offsetPosition;
     }
 }
